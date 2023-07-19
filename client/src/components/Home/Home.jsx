edea6cebd4403182305e7b0d524f026b30270513
vanhau2002 <vovanhau2002.qng@gmail.com>
2023-07-19 16:08:53 +0700

    Footer

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,15 @@
 import "./Home.scss";
+import Category from "./Category/Category";
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div>
+            <div className="main-content">
+                <div className="layout">
+                    <Category/>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Home;
