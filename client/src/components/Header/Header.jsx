8f48bb370dd5fa0041c59f10b5db0a41ab173d3d
TuNguyeDuyLong <tndragon01@gmail.com>
2023-07-20 12:25:35 +0700

    uploadcode

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,39 +12,45 @@ import { Context } from "../../utils/context";
 import "./Header.scss";
 
 const Header = () => {
-  const [scrolled, setScrolled] = useState(false);
-  const handleScroll = () => {
-    const offset = window.scrollY;
-    if (offset > 200) {
-      setScrolled(true);
-    } else {
-      setScrolled(false);
-    }
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-  }, []);
+    const [scrolled, setScrolled] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 200) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, []);
 
-  return (
-    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center">STORE</div>
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <span className="cart-icon">
-            <CgShoppingCart />
-            <span>5</span>
-          </span>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <>
+            <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+                <div className="header-content">
+                    <ul className="left">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center">STORE</div>
+                    <div className="right">
+                        <TbSearch onClick={() => setShowSearch(true)} />
+                        <AiOutlineHeart />
+                        <span className="cart-icon">
+                            <CgShoppingCart />
+                            <span>5</span>
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {showSearch && <Search setShowSearch={setShowSearch} />}
+        </>
+
+    );
 };
 
 export default Header;
+
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,30 +1,14 @@
 import "./Home.scss";
 
-<<<<<<< HEAD
-import Banner from './Banner/Banner';
 
 
-=======
+
 import Banner from "./Banner/Banner";
->>>>>>> f24030e4395728c46a97be09aa2541bcc1b58c77
+
 import Category from "./Category/Category";
 import Products from "../Products/Products";
 
 const Home = () => {
-<<<<<<< HEAD
-    return (
-
-        <div>
-            <Banner />
-            <div className="main-content">
-                <div className="layout">
-                    <Category />
-                </div>
-            </div>
-        </div>
-    )
-
-=======
   return (
     <div>
       <Banner />
@@ -36,7 +20,7 @@ const Home = () => {
       </div>
     </div>
   );
->>>>>>> f24030e4395728c46a97be09aa2541bcc1b58c77
+
 };
 
 export default Home;
